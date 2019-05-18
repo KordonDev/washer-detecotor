da8e1bb9e127ab701b301c7c5fc5aa2eb7948883
Arne Maier <Arne_Maier@gmx.de>
2019-05-18 16:31:52 +0000

    First commit to detect end of washing

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
- 
+const clapDetector = require('clap-detector'); 
+
+const clapConfig = {
+	AUDIO_SOURCE: 'alsa hw:1,0',
+	CLAP_AMPLITUDE_THRESHOLD: 0.25,
+	CLAP_ENERGY_THRESHHOLD: 0.3,
+	CLAP_MAX_DURATION: 1500,
+};
+
+clapDetector.start(clapConfig);
+
+clapDetector.onClap((history) => {
+	console.log('clap detected with');
+	console.dir(history);
+});
