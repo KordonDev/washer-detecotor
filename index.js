1f2948234b551a01f41d30f67340b76cc2917392
Arne Maier <Arne_Maier@gmx.de>
2020-05-10 14:04:55 +0200

    Use new version of audio detector

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const clapDetector = require('clap-detector');
+const audioDetector = require('clap-detector');
 const todoist = require('./todoist');
 
-const clapConfig = {
+const config = {
 	AUDIO_SOURCE: 'alsa hw:1,0',
 	CLAP_AMPLITUDE_THRESHOLD: 0.25,
 	CLAP_ENERGY_THRESHHOLD: 0.3,
 	CLAP_MAX_DURATION: 1500,
 };
 
-clapDetector.start(clapConfig);
+audioDetector.start(config);
 
-clapDetector.onClap((history) => {
+audioDetector.onDetections(3, 2000, (history) => {
 	console.log('clap detected with');
 	console.dir(history);
 	todoist()
 		.then(console.log);
 });
+console.log(new Date().toISOString(), 'washer detector started');
